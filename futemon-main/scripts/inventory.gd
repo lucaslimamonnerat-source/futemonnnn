c83e80fa8c54e3b88bccffa3bc9900805b736904
Rayrarpa <rafadevvv@gmail.com>
2026-09-02 15:03:21 -0300

    dsa

--- futemon-main/scripts/inventory.gd
+++ futemon-main/scripts/inventory.gd
@@ -6,3 +6,7 @@ extends CanvasLayer
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	inventory.visible = false
+
+func _process(delta: float) -> void:
+	if State.axe_state == 1:
+		$Control/NinePatchRect/Axe.visible = true
